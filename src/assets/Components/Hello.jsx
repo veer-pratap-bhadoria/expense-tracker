7db8dea18247897f1b513bb7c46259f584344514
veer <veerbhadoria12@gmail.com>
2026-06-29 19:47:02 +0530

    removed unnecessary comment

--- src/assets/Components/Hello.jsx
+++ src/assets/Components/Hello.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Hello() {
-    // const muthDays = ["monday", "wednesday", "thursday", "friday", "sunday"]
+    //removed comment slkfja;sj
     const person = [
         {
             name: "robert",
